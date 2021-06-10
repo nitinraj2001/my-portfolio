6d959e18d01efe3755d77accea9067aa2b7af373
Nitin Raj <nitinraj2001@gmail.com>
2021-06-11 00:24:13 +0530

    my portfolio commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
+import { ContactComponent } from './portfolio/contact/contact.component';
+import { SkillsComponent } from './portfolio/skills/skills.component';
+import { ProjectComponent } from './portfolio/project/project.component';
+import { HomeComponent } from './../../../customer-management/src/app/components/home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", component:HomeComponent, pathMatch:"full"},
+  {path:"home", component:HomeComponent},
+  {path:"projects", component:ProjectComponent},
+  {path:"skills", component:SkillsComponent},
+  {path:"contact", component:ContactComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,26 @@
+import { FooterComponent } from './portfolio/footer/footer.component';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule , CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './portfolio/navbar/navbar.component';
+import { HomeComponent } from './portfolio/home/home.component';
+import { SkillsComponent } from './portfolio/skills/skills.component';
+import { ProjectComponent } from './portfolio/project/project.component';
+import { ContactComponent } from './portfolio/contact/contact.component';
+import { ToolsComponent } from './portfolio/tools/tools.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    FooterComponent,
+    SkillsComponent,
+    ProjectComponent,
+    ContactComponent,
+    ToolsComponent
   ],
   imports: [
     BrowserModule,
